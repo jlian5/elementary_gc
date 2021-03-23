25d740c66b9573c8d86a61fdd76d1fa10622af42
Joe Lian <jlian5@illinois.edu>
2021-03-23 21:24:15 +0000

    used set to make sure unused_refs are all unique

--- stack_scan.c
+++ stack_scan.c
@@ -33,9 +33,13 @@ int main() {
 
 int f() {
     int* a = malloc(sizeof(int));
+    printf("a addr: %p\n", a);
     free(a);
     int* b = malloc(sizeof(int));
+    printf("b addr: %p\n", b);
+
     int* c = malloc(sizeof(int));
+    free(b);
     int* d = malloc(sizeof(int));
 
 
@@ -66,17 +70,19 @@ vector* unused_refs() {
         ptr++;
     }
 
-    vector* unused_refs = shallow_vector_create();
+    set* unused_refs = shallow_set_create();
     
     //scan through the stack frame to be cleaned up and see if any unsaved refs exist.
     ptr = curr_stack;
     while(ptr < caller_stack) {
         if(*ptr >=  base_heap && *ptr < curr_heap) {
-            if(!set_contains(caller_refs, *ptr)) vector_push_back(unused_refs, *ptr);
+            if(!set_contains(caller_refs, *ptr) && !set_contains(unused_refs, *ptr)) 
+                set_add(unused_refs, *ptr);
         }
         ptr++;
     }
+    vector* unused_refs_vec = set_elements(unused_refs);
     set_destroy(caller_refs);
 
-    return unused_refs; //TODO used this vector to see if any of these unused_refs point to used memory, if so free them.
+    return unused_refs_vec; //TODO used this vector to see if any of these unused_refs point to used memory, if so free them.
 }
